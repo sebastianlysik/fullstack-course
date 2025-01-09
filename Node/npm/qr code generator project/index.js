360b39748a6537b5fd9460bc9fb49289c1a7d54f
sebastianlysik <lysik.seb@gmail.com>
2025-01-09 20:24:16 +0100

    made an app based on npm packages to make a qr code out of a link

--- Node/npm/qr code generator project/index.js
+++ Node/npm/qr code generator project/index.js
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
 .prompt([
@@ -7,12 +9,15 @@ inquirer
     name: "URL"
 }])
   .then((answers) => {
-    console.log(answers);
+    const url = answers.URL; 
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
+
+    }
+     else {
       // Something else went wrong
     }
   });
