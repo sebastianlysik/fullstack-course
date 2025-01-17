e24e479982537923f4ae2f43d6865fa395d00ed4
sebastianlysik <lysik.seb@gmail.com>
2025-01-17 09:09:30 +0100

    practising some styling in react

---
+++ React/2nd/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  const d = new Date();
+  let hour = d.getHours();
+  let greeting = " "; 
+  const customStyle = {
+    color: "black"
+  }
+
+  if(hour < 12){
+    greeting = "Good morning";
+    customStyle.color = "red";
+  }
+  else if(hour > 18){
+    greeting = "Good afternoon";
+    customStyle.color = "green";
+  }
+  else{
+    greeting = "Good night"
+    customStyle.color = "blue";
+  }
+
+  return (
+    <>
+      <h1 style={customStyle}>{greeting}</h1>
+    </>
+  )
+}
+
+export default App
